781740b7b009cb91bc78c1fafd1234a66feddbee
Voldemortik <lesnoydyadya@gmail.com>
2021-03-06 12:07:01 +0200

    aa

---
+++ function.js
@@ -0,0 +1,8 @@
+$(document.getElementById('').click(function () {
+    $(".web_designs").hide();
+    $(".icon_contents").show()
+}));
+$(".btn2").click(function () {
+             $(".second_div").hide();
+             $(".first_div").show()
+     });
